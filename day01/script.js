af7d149bbba314a550db876f9f07319a77cbe160
yadaykim <yadaykim@gmail.com>
2026-08-21 18:04:11 +0900

    feat: 01. 영화 데이터 변수 만들기

---
+++ day01/script.js
@@ -0,0 +1,6 @@
+const title = "인터스텔라"; //영화 제목
+const voteAverage = 8.7; //평균 평점
+const voteCount = 32000; //평가 수
+const popularity = 150.5; //인기도 점수
+const releaseDate = "2014-11-05"; //개봉일
+const originalLanguage = "en"; //원어
