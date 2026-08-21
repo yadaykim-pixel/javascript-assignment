e435e583467032dbff9d2c2005bb5e4c1eb355dc
yadaykim <yadaykim@gmail.com>
2026-08-21 18:08:09 +0900

    02. 영화 데이터 Console에 출력하기

--- day01/script.js
+++ day01/script.js
@@ -4,3 +4,10 @@ const voteCount = 32000; //평가 수
 const popularity = 150.5; //인기도 점수
 const releaseDate = "2014-11-05"; //개봉일
 const originalLanguage = "en"; //원어
+
+console.log(title);
+console.log(voteAverage);
+console.log(voteCount);
+console.log(popularity);
+console.log(releaseDate);
+console.log(originalLanguage);
