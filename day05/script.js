807b893779d200f581423484c8a36356dba52ea4
yadaykim <yadaykim@gmail.com>
2026-08-27 17:18:27 +0900

    feat: day05 commit 완료

--- day05/script.js
+++ day05/script.js
@@ -5,3 +5,33 @@ const movies = [
   {title: "테넷", voteAverage: 7.3,},
 ];
 
+
+// 2. find로 영화 찾기
+const foundMovie = movies.find((movie) => movie.title === "인터스텔라");
+if (foundMovie) {
+  console.log(`${foundMovie.title}의 평점은 ${foundMovie.voteAverage}점입니다.`);}
+
+
+// 3. filter()로 평점이 높은 영화 찾기
+const highScoreMovies = movies.filter((movie) => movie.voteAverage >= 8.5);
+console.log(highScoreMovies);
+
+
+// 4. map()으로 영화 제목만 가져오기
+const movieTitles = movies.map((movie) => movie.title);
+console.log(movieTitles);
+
+// 5. 검색어 가공하기
+const searchForm = document.querySelector('form');
+const searchInput = document.querySelector('input');
+const searchResult = document.querySelector('#search-result');
+
+searchForm.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  const keyword = searchInput.value.trim().toLowerCase();
+
+  searchResult.textContent = `검색어: ${keyword}`;
+});
+
+
