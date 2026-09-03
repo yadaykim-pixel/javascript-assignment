9ad7cc5faca2ee49bc64c23adff067e670b5d8d2
yadaykim <yadaykim@gmail.com>
2026-09-03 20:15:00 +0900

    DAY20. JavaScript로 통신 오류 처리하기

--- day10/script.js
+++ day10/script.js
@@ -34,6 +34,7 @@ function createMovieCard(movie) {
 }
 
 function renderMovies(movies) {
+  container.textContent = ""; // Loading 상태 지우기
   movies.forEach((movie) => {
     container.append(createMovieCard(movie));
   });
@@ -41,17 +42,33 @@ function renderMovies(movies) {
 
 async function getPopularMovies() {
   // TODO 1. Loading 상태를 표시하세요.
+  container.innerHTML = "Loading...";
 
   // TODO 2. try / catch 구조를 작성하세요.
   try {
     // TODO 3. fetch()와 await를 사용해 Response를 받으세요.
-    // TODO 4. response.ok가 false라면 오류 안내 문구를 표시하고 return으로 함수를 종료하세요.
+    const response = await fetch(URL, options);
+
+    // TODO 4. response.ok가 false라면 오류 안내 문구를 표시하고 return으로 함수를 종료하세요. (HTTP 오류 처리)
+    if (!response.ok) {
+      throw new Error(`HTTP 오류: 상태 코드 ${response.status}`);
+    }
+
     // TODO 5. response.json()으로 데이터를 변환하세요.
-    // TODO 6. Loading 문구를 지우세요.
+    const data = await response.json();
+
+    // TODO 6. Loading 문구를 지우세요. (성공 시)
+    container.textContent = "";
+
     // TODO 7. data.results를 renderMovies()에 전달하세요.
+    renderMovies(data.results);
   } catch (error) {
+   
     // TODO 8. 사용자에게 오류 안내 문구를 표시하세요.
-    // TODO 9. console.error(error)로 실제 오류를 출력하세요.
+    console.error("API 호출 중 오류 발생:", error);
+    container.innerHTML = "영화 정보를 불러오지 못했습니다.";
+
+    // TODO 9. console.error(error)로 실제 오류를 출력하세요. (이미 위에서 실행됨)
   }
 }
 
